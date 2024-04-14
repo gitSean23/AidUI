0d995a0942e0c44155013bc0905206c498195622
Greesh12 <134640893+Greesh12@users.noreply.github.com>
2024-04-14 14:19:48 -0400

    Update ocr.py

--- UIED/detect_text/ocr.py
+++ UIED/detect_text/ocr.py
@@ -24,8 +24,8 @@ def Google_OCR_makeImageData(imgpath):
 
 def ocr_detection_google(imgpath):
     start = time.clock()
-    url = 'https://vision.googleapis.com/v1/images:annotate'
     # api_key = 'AIzaSyDUc4iOUASJQYkVwSomIArTKhE2C6bHK8U'             # *** Replace with your own Key ***
+    api_key = 'AIzaSyAeSaaOE-upsRshfOEkkMIUcAiBzDSVOAo'
     api_key = 'AIzaSyBG21ADvuW70fPnVzsx9ZnuHdh9Ma7vess'
     imgdata = Google_OCR_makeImageData(imgpath)
     response = requests.post(url,
