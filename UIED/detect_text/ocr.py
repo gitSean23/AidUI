f9a14d8d97359786f5d4c6c03a60aa0afedc10d7
Greesh12 <greeshwanthp@gmail.com>
2024-04-14 23:14:30 -0400

    added a new file to git ignore.

--- UIED/detect_text/ocr.py
+++ UIED/detect_text/ocr.py
@@ -27,7 +27,6 @@ def ocr_detection_google(imgpath):
     url = 'https://vision.googleapis.com/v1/images:annotate'
     # api_key = 'AIzaSyDUc4iOUASJQYkVwSomIArTKhE2C6bHK8U'             # *** Replace with your own Key ***
     api_key = 'AIzaSyAeSaaOE-upsRshfOEkkMIUcAiBzDSVOAo'
-    api_key = 'AIzaSyBG21ADvuW70fPnVzsx9ZnuHdh9Ma7vess'
     imgdata = Google_OCR_makeImageData(imgpath)
     response = requests.post(url,
                              data=imgdata,
